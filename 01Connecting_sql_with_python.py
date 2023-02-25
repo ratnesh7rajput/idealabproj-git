89b5ee2920dbd8067b28205806fd83cb78f3db28
Prateekk02 <99165065+Prateekk02@users.noreply.github.com>
2023-02-25 12:16:37 +0530

    Update 01Connecting_sql_with_python.py
    Description of mysql.connector.connect () is done.

--- 01Connecting_sql_with_python.py
+++ 01Connecting_sql_with_python.py
@@ -2,9 +2,9 @@ import mysql.connector
 # import mysql.connector
 #create user 'user'@'%' identified by 'password'
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="abc",
-  password="password"
+  host="contains the url of the source database",
+  user="contains the user name",
+  password="password of the source database"
 )
 print(mydb)
 mycursor = mydb.cursor()
