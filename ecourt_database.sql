95e91bf28da34e2f7ab754611108e9e301004539
Aryan Singh <122144176+aryansingh81@users.noreply.github.com>
2023-03-14 16:44:43 +0530

    Add files via upload
    Final Database

---
+++ ecourt_database.sql
@@ -0,0 +1,108 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ecourt_database
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cri_case`
+--
+
+DROP TABLE IF EXISTS `cri_case`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cri_case` (
+  `Aadhar_no` bigint NOT NULL,
+  `Case_id` varchar(50) NOT NULL,
+  `Section` varchar(50) NOT NULL,
+  `Status` varchar(50) NOT NULL,
+  `s_no` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cri_case`
+--
+
+LOCK TABLES `cri_case` WRITE;
+/*!40000 ALTER TABLE `cri_case` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cri_case` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cri_d`
+--
+
+DROP TABLE IF EXISTS `cri_d`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cri_d` (
+  `Name` char(50) NOT NULL,
+  `Address` varchar(300) NOT NULL,
+  `phone_no` bigint NOT NULL,
+  `gender` char(7) NOT NULL,
+  `dob` varchar(15) NOT NULL,
+  `Aadhar_no` bigint NOT NULL,
+  PRIMARY KEY (`Aadhar_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cri_d`
+--
+
+LOCK TABLES `cri_d` WRITE;
+/*!40000 ALTER TABLE `cri_d` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cri_d` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `plar`
+--
+
+DROP TABLE IF EXISTS `plar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `plar` (
+  `Name` char(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Phone_no` bigint NOT NULL,
+  `Police_no` varchar(50) NOT NULL,
+  `Aadhar_no` bigint NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  PRIMARY KEY (`Police_no`),
+  UNIQUE KEY `Aadhar_no_UNIQUE` (`Aadhar_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `plar`
+--
+
+LOCK TABLES `plar` WRITE;
+/*!40000 ALTER TABLE `plar` DISABLE KEYS */;
+INSERT INTO `plar` VALUES ('Aryan Singh','singharyan262003@gmail.com',9027243423,'POL0801',434850584712,'mysql1@');
+/*!40000 ALTER TABLE `plar` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-14 16:42:36
